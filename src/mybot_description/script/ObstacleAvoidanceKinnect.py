9b242a75b18b2e644f69097717fb32db56ed373f
Anmol Agarwal <57227202+anmolag10@users.noreply.github.com>
2021-05-04 13:04:06 +0530

    code refactoring

--- src/mybot_description/script/ObstacleAvoidanceKinnect.py
+++ src/mybot_description/script/ObstacleAvoidanceKinnect.py
@@ -1,247 +1,167 @@
 #! /usr/bin/env python
 
 import rospy
-
 from sensor_msgs.msg import LaserScan, Imu, NavSatFix
 from geometry_msgs.msg import Twist
 from tf.transformations import euler_from_quaternion
 import math
 import time
 from pyproj import Geod
-yaw=0
-gps_angle=0
-lat1=0
-lon1=0
-dist=0
-lat2= 49.9000015169
-state_description=''
-start=True
-lon2= 8.89987335398
-state=0
-regions={}
 
 
+class Avoidance():
+    def __init__(self):
+        self.yaw = 0
+        self.gps_angle = 0
+        self.lat1 = 0
+        self.lon1 = 0
+        self.dist = 0
+        self.lat2 = 49.9000015169
+        self.state_description = ''
+        self. start = True
+        self.lon2 = 8.89987335398
+        self.state = 0
+        self.regions = {}
+        self.pub = None
 
-pub = None
+    def imu_callback(self, pose):
+        quaternion = (pose.orientation.x, pose.orientation.y,
+                      pose.orientation.z, pose.orientation.z)
+        euler = euler_from_quaternion(quaternion)
+        self.yaw = math.degrees(euler[2])+180
+        self.yaw = abs(yaw-360)
+        self.yaw = yaw % 360
 
-def callback(pose):
-    global yaw
-    quaternion = (pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w)
+    def gps_callback(self, data):
+        self.lat1 = data.latitiude
+        self.lon2 = data.longitude
 
-    euler = euler_from_quaternion(quaternion)
-    yaw= math.degrees(euler[2])+180
-    yaw = abs(yaw-360)
-    yaw = yaw%360
+    def align(self):
+        speed_msg = Twist()
+        self.dist
+        geodesic = Geod(ellps='WGS84')
+        bearing, reverse_bearing, dist = geodesic.inv(
+            self.lon1, self.lat1, self.lon2, self.lat2)
+        bearing = bearing + 180
+        angle_diff = self.yaw - bearing
+        while((angle_diff > 1 or angle_diff < -1)):
+            if angle_diff > 1:
+                if angle_diff < 180:
+                    speed_msg.angular.z = 0.7
+                elif angle_diff > 180:
+                    speed_msg.angular.z = -0.7
+                pub.publish(speed_msg)
 
-def callback2(data):
-    global lat1 
-    lat1= data.latitude
-    global lon1 
-    lon1= data.longitude
-def angle_dif():
-   global dist
-   geodesic =Geod(ellps='WGS84')
-   bearing, reverse_bearing, dist = geodesic.inv(lon1,lat1,lon2,lat2)
-   bearing = bearing +180
-   angle_diff = yaw- bearing
-   return angle_diff
-   
+            elif angle_diff < -1:
+                angle_diff = abs(angle_diff)
+                if angle_diff < 180:
+                    speed_msg.angular.z = -0.7
+                elif angle_diff > 180:
+                    speed_msg.angular.z = 0.7
+                pub.publish(speed_msg)
 
+    def take_actions(self):
+        speed_msg = Twist()
+        linear_x = 0
+        angular_z = 0
 
+        if self.regions['front'] > 1 and self.regions['fleft'] > 1 and self.regions['fright'] > 1:
+            self.state_description = '  CASE 1 - CLEAR'
+            linear_x = 0
+            angular_z = 0
+            self.state = 0
 
+        elif self.regions['front'] < 1 and self.regions['fleft'] > 1 and self.regions['fright'] > 1:
+            self.state_description = 'CASE 2 - FRONT OBSTRUCTED'
+            self.state = 1
+            linear_x = 0
+            angular_z = -0.3
 
-def take_action(regions):
-    msg = Twist()
-    linear_x = 0
-    angular_z = 0
-    global state
+        elif self.regions['front'] > 1 and self.regions['fleft'] > 1 and self.regions['fright'] < 1:
+            self.state_description = 'CASE 3 - FRIGHT OBSTRUCTED'
+            self.state = 1
+            linear_x = 0.0
+            angular_z = -0.3
 
-    global state_description
-    anglediff=angle_dif()
-    if regions['front'] > 1 and regions['fleft'] > 1 and regions['fright'] > 1:
-     state_description = 'case 1 - nothing'
-     linear_x = 0.0
-     angular_z = 0
-     state=0
-   
-    elif regions['front'] < 1 and regions['fleft'] > 1 and regions['fright'] > 1:
-        state_description = 'case 2 - front'
-        linear_x = 0.0
-        angular_z = -0.3
-        
-        
+        elif self.regions['front'] > 1 and self.regions['fleft'] < 1 and self.regions['fright'] > 1:
+            self.state_description = 'CASE 4 -FLEFT OBSTRUCTED'
+            self.state = 1
+            linear_x = 0.0
+            angular_z = 0.3
 
-    elif regions['front'] > 1 and regions['fleft'] > 1 and regions['fright'] < 1:
-        state_description = 'case 3 - fright'
-        linear_x = 0.0
-        angular_z = -0.3
-        
-    elif regions['front'] > 1 and regions['fleft'] < 1 and regions['fright'] > 1:
-        state_description = 'case 4 - fleft'
-        linear_x = 0.0
-        angular_z = 0.3
-        
-    elif regions['front'] < 1 and regions['fleft'] > 1 and regions['fright'] < 1:
-        state_description = 'case 5 - front and fright'
-        linear_x = 0.0
-        angular_z = -0.3
-        
-    elif regions['front'] < 1 and regions['fleft'] < 1 and regions['fright'] > 1:
-        state_description = 'case 6 - front and fleft'
-        linear_x = 0.0
-        angular_z = 0.3
-        
-    elif regions['front'] < 1 and regions['fleft'] < 1 and regions['fright'] < 1:
-        state_description = 'case 7 - front and fleft and fright'
-        linear_x = 0.0
-        angular_z = -0.3
-      
-    elif regions['front'] > 1 and regions['fleft'] < 1 and regions['fright'] < 1:
-        state_description = 'case 8 - fleft and fright'
-        linear_x = 0.0
-        angular_z = -0.3
-        
-    else:
-        state_description = 'unknown case'
-        rospy.loginfo(regions)
+        elif self.regions['front'] < 1 and self.regions['fleft'] > 1 and self.regions['fright'] < 1:
+            self.state_description = 'CASE 5 - FRONT AND FRIGHT OBSTRUCTED'
+            self.state = 1
+            linear_x = 0.0
+            angular_z = -0.3
 
-    rospy.loginfo(state_description)
-    msg.linear.x = linear_x
-    msg.angular.z = angular_z
-    pub.publish(msg)
-def clbk_laser(msg):
-      global start
-      global state_description
-      print(start)
-      twist=Twist()
-      
-      if str(min(msg.ranges[0:144]))=='nan':
-        r1=10
-      else:
-        r1=(min(msg.ranges[0:144]))
-      if str(min(msg.ranges[144:287]))=='nan':
-        r2=10
-      else:
-        r2=(min(msg.ranges[144:287]))
-      if str(min(msg.ranges[288:431]))=='nan':
-        r3=10
-      else:
-        r3=(min(msg.ranges[288:431]))
-      if str(min(msg.ranges[432:575]))=='nan':
-        r4=10
-      else:
-        r4=(min(msg.ranges[432:575]))
-      if str(min(msg.ranges[576:713]))=='nan':
-        r5=10
-      else:
-        r5=(min(msg.ranges[576:713]))   
-      global regions     
-      regions = {
-        'right':  min(r5, 10),
-        'fright': min(r4, 10),
-        'front':  min(r3, 10),
-        'fleft':  min(r2, 10),
-        'left':   min(r1, 10), }
-      
-      angle_diff=angle_dif()
-      print(angle_diff)
-      print('start val',start)
-      
-      while (angle_diff>2 or angle_diff<-2) and  (start is True):
-       
-       print('entering the loop')
-       angle_diff=angle_dif()
-       if angle_diff>2:
-            print(2)
-            print(angle_diff)
-            if angle_diff<180:
-               twist.angular.z=0.7
-            elif angle_diff>180:
-                twist.angular.z=-0.7
-            pub.publish(twist)
+        elif self.regions['front'] < 1 and self.regions['fleft'] < 1 and self.regions['fright'] > 1:
+            self.state_description = 'CASE 6 - FRONT AND FLEFT OBSTRUCTED'
+            self.state = 1
+            linear_x = 0.0
+            angular_z = 0.3
 
-       elif angle_diff<-2:
-            print(3)
-            print(angle_diff)
-            angle_diff = abs(angle_diff)
-            if angle_diff<180:
-                twist.angular.z = -0.7
-            elif angle_diff>180:
-                twist.angular.z = 0.7
-            pub.publish(twist)
-       if angle_diff<2 and angle_diff>-2:
+        else:
+            self.state_description = 'LOCAL MINIMA HAS OCCURED'
+            rospy.loginfo(self.regions)
 
-           twist.angular.z=0
-           print(angle_diff)
-           pub.publish(twist)
-           start=False
-           break
-      
-def main():
-    global pub
- 
-    rospy.init_node('reading_laser','bot_yaw','bot_gps')
-    
-    rospy.Subscriber("imu",Imu,callback)
-    rospy.Subscriber("fix",NavSatFix,callback2)
-    rate=rospy.Rate(10)
-    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-    sub = rospy.Subscriber('/scan', LaserScan, clbk_laser)
-    twist=Twist()
-    while state ==0:
-     while 1:
-	geodesic =Geod(ellps='WGS84')
-	bearing, reverse_bearing, dist = geodesic.inv(lon1,lat1,lon2,lat2)
-        bearing = bearing +180
-	angle_diff = yaw- bearing
-        print("Yaw: ", yaw, "Bearing: ", bearing)
-        print("Angle: ", angle_diff)       
-        
-        if angle_diff>1:
-            if angle_diff<180:
-                twist.angular.z=0.7
-            elif angle_diff>180:
-                twist.angular.z=-0.7
-            pub.publish(twist)
+        rospy.loginfo(self.state_description)
+        speed_msg.linear.x = linear_x
+        speed_msg.angular.z = angular_z
+        pub.publish(speed_msg)
 
-        elif angle_diff<-1:
-            angle_diff = abs(angle_diff)
-            if angle_diff<180:
-                twist.angular.z = -0.7
-            elif angle_diff>180:
-                twist.angular.z = 0.7
-            pub.publish(twist) 
-        if angle_diff<1 and angle_diff>-1:
-            break
-     while 1:
-        bearing, reverse_bearing, dist = geodesic.inv(lon1,lat1,lon2,lat2)
-        if  dist>3:
-            twist.linear.y = 0
-            twist.linear.z = 0
-            twist.angular.x=0
-            twist.angular.y=0
-            twist.angular.z=0
-            twist.linear.x= -0.8
-            print("Distance: ", dist)
-            pub.publish(twist)
-            flag=0
-        
-        elif dist<3:
-            twist.linear.y = 0
-            twist.linear.z = 0
-            twist.angular.x=0
-            twist.angular.y=0
-            twist.angular.z=0
-            twist.linear.x = 0
-            pub.publish(twist)     
-   
-    while state==1:
-       take_action(regions)
+    def kinect_callback(self, msg):
+        newranges = []
+        for i in range(0, 720):
+            newranges.append(msg.ranges[i])
+            if math.isnan(msg.ranges[i]) is True:
+                newranges[i] = 10
+
+        regions = {'right': min(min(newranges[576:713], 10)),
+                   'fright': min(min(newranges[4376:575], 10)),
+                   'front':  min(min(newranges[288:431]), 10),
+                   'fleft':  min(min(newranges[144:287]), 10),
+                   'left':   min(min(newranges[0:143]), 10)}
+
+        if self.start is True:
+            self.align()
+            self.start = False
+
+        self.take_actions(self.regions)
+
+    def speed_cmd(self, l_x, l_y, l_z, a_x, a_y, a_z):
+        speed_msg = Twist()
+
+        speed_msg.linear.x = l_x
+        speed_msg.linear.y = l_y
+        speed_msg.linear.z = l_x
+        speed_msg.angular.x = a_x
+        speed_msg.angular.y = a_y
+        speed_msg.angular.z = a_z
+        self.pub.publish(speed_msg)
+
+    def main_function(self):
+        rospy.init_node('Obstacle Avoidance')
+        rospy.Subscriber('imu', Imu, imu_callback)
+        rospy.Subscriber('fix', NavSatFix, gps_callback)
+        rospy.Subscriber('/scan', LaserScan, kinect_callback)
+
+        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+
+        while self.state == 0:
+            self.align
+            while True:
+                _, _, self.dist = geodesic.inv(
+                    self.lon1, self.lat1, self.lon2, self.lat2)
+                if self.dist > 1.5:
+                    self.speed_cmd(-0.8, 0, 0, 0, 0, 0)
+                else:
+                    self.speed_cmd(0, 0, 0, 0, 0, 0)
+        while self.state == 1:
+            self.take_actions(self.regions)
+        rospy.spin()
 
 
-
- 
-    rospy.spin()
- 
 if __name__ == '__main__':
-    main()
+    Traversal = Avoidance()
+    Traversal.main_function()
